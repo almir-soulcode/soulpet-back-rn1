7077631118e5800aca4d5e9c327481b5b87e9827
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-04-27 15:20:14 -0300

    Criação do model de serviço

---
+++ database/servico.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const Pet = require("./pet");
+
+const Servico = connection.define("servico", {
+  
+  nome: {
+    type: DataTypes.STRING(130),
+    allowNull: false, 
+  },
+  preco: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }, 
+  status: {
+    type: DataTypes.STRING,
+    allowNull:false,
+    defaultValue: "pendente"
+  },
+  data: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  }
+  
+});
+
+
+//Relacionamento - um pet pode ter vários serviços e o serviço pertence a um único pet
+Servico.hasMany(Pet,{onDelete:"CASCADE"});
+Pet.belongsTo(Servico);
+
+
+
+module.exports = Servico;
